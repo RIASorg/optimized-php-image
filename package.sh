a9509f76e3b2b26dfb91d364bd5b9e7a98d245b0
L3tum <9307432+L3tum@users.noreply.github.com>
2024-03-24 01:19:40 +0100

    Update package.sh

--- package.sh
+++ package.sh
@@ -10,6 +10,8 @@ while read -r in; do cp -Lr --parents --preserve=links "$in" /php/ 2>/dev/null |
 if [ "$1" = "scratch" ]
 then
   printf "Packaging for scratch"
+  # OPCache or APCu need /tmp available for some kind of lock (???)
+  mkdir -p /php/tmp
 else
   printf "Packaging for Linux distro"
   # - Move /lib and /lib64, since those are actually symbolic links that are present on basically every Linux installation and overwriting them is a big no-no
@@ -17,9 +19,6 @@ else
   mv /php/lib/* /php/usr/lib/ && mv /php/lib64/* /php/usr/lib64/
   rm -rf /php/lib && rm -rf /php/lib64
 fi
-
-# OPCache or APCu need /tmp available for some kind of lock (???)
-mkdir -p /php/tmp
 
 # A lot of extensions (for example, rdkafka) will need the certificates locally available
 # Since they are fairly small overall, we can copy them over regardless of extension
